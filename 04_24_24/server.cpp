f0610cfde5cd4f17479f488450f5185eba9cd169
Angela <avenable11@ivytech.edu>
2024-04-24 19:13:56 -0400

    partial server

---
+++ 04_24_24/server.cpp
@@ -0,0 +1,29 @@
+#include "PracticalSocket.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    try
+    {
+        TCPServerSocket servSock(9431);
+        for (;;)
+        {
+            TCPSocket *sock = servSock.accept();
+            u_int32_t val;
+            if (sock->recvFully(&val, sizeof(val)) == sizeof(val))
+            {
+                val = ntohl(val);
+                val++;
+                val = htonl(val);
+                sock->send(&val, sizeof(val));
+            }
+        }
+    }
+    catch (SocketException e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
+}
